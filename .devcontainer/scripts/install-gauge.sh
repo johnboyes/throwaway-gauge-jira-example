cb2297667e291a4081b5aa85111e752202828617
John Boyes <154404+johnboyes@users.noreply.github.com>
2021-03-16 12:22:17 +0000

    Install Gauge Python plugin

--- .devcontainer/scripts/install-gauge.sh
+++ .devcontainer/scripts/install-gauge.sh
@@ -6,5 +6,5 @@ TRUNCATED_GAUGE_VERSION="${GAUGE_VERSION:1}"
 wget https://github.com/getgauge/gauge/releases/download/$GAUGE_VERSION/gauge-$TRUNCATED_GAUGE_VERSION-linux.x86_64.zip
 unzip -o gauge-$TRUNCATED_GAUGE_VERSION-linux.x86_64.zip -d /usr/local/bin
 su codespace -c "gauge install html-report"
-# su codespace -c "gauge install python"
+su codespace -c "gauge install python"
 # add more plugins here as required
